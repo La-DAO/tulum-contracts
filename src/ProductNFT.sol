1b616aaa8a05ae8cbdf67d615138fb815171e426
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-12-28 19:56:25 -0600

    fix: cleared git cache for .ignored and solidity compiling errors

--- src/ProductNFT.sol
+++ src/ProductNFT.sol
@@ -8,61 +8,64 @@ pragma solidity 0.8.15;
  */
 
 import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721URIStorage} from
+  "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Counters} from "../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract ProductNFT is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+  Counters.Counter private _tokenIdCounter;
 
-    // Token name
-    string private _name;
-    // Token symbol
-    string private _symbol;
-    uint256 maxSupply;
+  // Token name
+  string private _name;
+  // Token symbol
+  string private _symbol;
+  uint256 maxSupply;
 
-    constructor(string memory name_, string memory symbol_, uint8 _maxSupply) ERC721("Product NFT", "PRDNFT") {
-        _name = name_;
-        _symbol = symbol_;
-        maxSupply = _maxSupply;
-    }
+  constructor(
+    string memory name_,
+    string memory symbol_,
+    uint8 _maxSupply
+  )
+    ERC721("Product NFT", "PRDNFT")
+  {
+    _name = name_;
+    _symbol = symbol_;
+    maxSupply = _maxSupply;
+  }
 
-    // Safe mint is called by ProjectEscrow contract and NFT is owned by ProjectEscrow?
-    function safeMint(address to, string memory uri) public onlyOwner { // ProjectEscrow address como admin
-        require(_tokenIdCounter.current() < maxSupply, "NFT max supply has been reached");
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
+  // Safe mint is called by ProjectEscrow contract and NFT is owned by ProjectEscrow?
+  function safeMint(address to, string memory uri) public onlyOwner {
+    // ProjectEscrow address como admin
+    require(_tokenIdCounter.current() < maxSupply, "NFT max supply has been reached");
+    uint256 tokenId = _tokenIdCounter.current();
+    _tokenIdCounter.increment();
+    _safeMint(to, tokenId);
+    _setTokenURI(tokenId, uri);
+  }
 
-    function setTokenURI(uint256 tokenId, string memory newURI) public {
-        address tokenOwner  = ownerOf(tokenId);
-        require(msg.sender == tokenOwner, "Caller is not the owner or an approved operator");
-        _setTokenURI(tokenId, newURI);
-    }
+  function setTokenURI(uint256 tokenId, string memory newURI) public {
+    address tokenOwner = ownerOf(tokenId);
+    require(msg.sender == tokenOwner, "Caller is not the owner or an approved operator");
+    _setTokenURI(tokenId, newURI);
+  }
 
-    function _burn(uint256 tokenId) internal onlyOwner override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
+  function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) onlyOwner {
+    super._burn(tokenId);
+  }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override (ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function supportsInterface(bytes4 interfaceId) public view override (ERC721) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
 }
